cccbf79531998ac950cceb53250ddc9923c3ef94
EunJi Kim <dust9629@gmail.com>
2024-03-18 23:59:44 +0900

    Feat add css

--- my-new-design/src/Components/WeatherApp/WeatherApp.jsx
+++ my-new-design/src/Components/WeatherApp/WeatherApp.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './WeatherApp.css';
 
+import search_icon from '../Assets/';
+import drizzle_icon from '../Assets/drizzle.png';
+import rain_icon from '../Assets/rain.png';
+import snow_icon from '../Assets/snow.png';
+import wind_icon from '../Assets/wind.png';
+import humidity_icon from '../Assets/humidity.png';
+
 const WeatherApp = () => {
   return (
-    <div>
-
+    <div className='container'>
+      <div className='top-bar'>
+        <input type="text" className='cityInput' placeholder='' />
+        <div className='search-icon'>
+          <img src={search_icon} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
