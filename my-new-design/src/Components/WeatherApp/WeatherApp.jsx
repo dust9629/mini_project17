53175c9ba89f2f70736c71f9781fd2f9c86f5295
EunJi Kim <dust9629@gmail.com>
2024-03-19 23:18:55 +0900

    Feat : add img files and jsx file

--- my-new-design/src/Components/WeatherApp/WeatherApp.jsx
+++ my-new-design/src/Components/WeatherApp/WeatherApp.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import './WeatherApp.css';
+import '.WeatherApp/WeatherApp.css';
 
-import search_icon from '../Assets/';
-import drizzle_icon from '../Assets/drizzle.png';
-import rain_icon from '../Assets/rain.png';
-import snow_icon from '../Assets/snow.png';
+import search_icon from '../Assets/search.png';
+// import clear_icon from '../Assets/clear.png';
+import cloud_icon from '../Assets/cloud.png';
+// import drizzle_icon from '../Assets/drizzle.png';
+// import rain_icon from '../Assets/rain.png';
+// import snow_icon from '../Assets/snow.png';
 import wind_icon from '../Assets/wind.png';
-import humidity_icon from '../Assets/humidity.png';
+// import humidity_icon from '../Assets/humidity.png';
 
 const WeatherApp = () => {
   return (
@@ -15,6 +17,27 @@ const WeatherApp = () => {
         <input type="text" className='cityInput' placeholder='' />
         <div className='search-icon'>
           <img src={search_icon} alt="" />
+        </div>
+      </div>
+      <div className="weather-image">
+        <img src={cloud_icon} alt=''/>
+      </div>
+      <div className="weather-temp">24°C</div>
+      <div className="weather-location">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img src="" alt="" className='icon' />
+          <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={wind_icon} alt="" className='icon' />
+          <div className="data">
+            <div className="humidity-percent">18 km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
         </div>
       </div>
     </div>
